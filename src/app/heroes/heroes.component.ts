1087c63c404cb6cbb5094c889f99b51c603df8d6
Michael Cassie <Macpenguin0@gmail.com>
2019-02-18 09:10:07 -0500

    Third Commit: Return of the Commit

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
 
 @Component({
   selector: 'app-heroes',
@@ -7,6 +9,9 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+
+  heroes = HEROES;
+
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
@@ -16,4 +21,10 @@ export class HeroesComponent implements OnInit {
   ngOnInit() {
   }
 
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
 }
+
